5f5a73cfeb9e02da5cc65fe9c079645c925e01c0
wendytaya <wendy.taya@ucsp.edu.pe>
2016-03-27 15:58:34 -0500

    .

--- ejercicio 1.4.c
+++ ejercicio 1.4.c
@@ -3,14 +3,14 @@ main()
 {
 	int fahr , celsius;
 	int lower , upper , step;
-	lower=0;
-	upper =300;
+	lower=300;
+	upper =0;
 	step=20;
 	fahr =lower;
 	printf ( "CELSIUS - FAHRENHEIT \n" );
-	while(celsius <= upper){
+	while(fahr>=upper ){
 		fahr =(9*celsius +160)/5;
 		printf ("%d\t       %d\n", celsius, fahr);
-		celsius=celsius +step;
+		celsius=celsius -step;
 	}
 }
--- ipoo teoria/ejercicio8.c
+++ ipoo teoria/ejercicio8.c
@@ -3,26 +3,26 @@
 #include <stdio.h>
 main()
 {
- int a, b, c;
- printf (" Introduzca tres numeros.\n");
- scanf ("%d %d %d", &a, &b, &c);
- if (a<b)
- {
- if (a<c)
- {
- if (b<c)
- printf (" El orden es %d, %d, %d. \n",a,b,c);
- else printf (" El orden es %d, %d, %d.\n",a,c,b);
- }
- }
- else
- {
- if (b<c)
- {
- if (a<c)
- printf (" El orden es %d, %d, %d.\n",b,a,c);
- else printf (" El orden es %d, %d, %d.\n",b,c,a);
- }
- }
+    int a, b, c;
+    printf (" Introduzca tres numeros.\n");
+	scanf ("%d %d %d", &a, &b, &c);
+	if (a<b)
+	{
+		if (a<c)
+		{
+			if (b<c)
+			printf (" El orden es %d, %d, %d. \n",a,b,c);
+			else printf (" El orden es %d, %d, %d.\n",a,c,b);
+		}
+	}
+	else
+	{
+		if (b<c)
+		{
+			if (a<c)
+			printf (" El orden es %d, %d, %d.\n",b,a,c);
+			else printf (" El orden es %d, %d, %d.\n",b,c,a);
+		}
+	}
 }
 
