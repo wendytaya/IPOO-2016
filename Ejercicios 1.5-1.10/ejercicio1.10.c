f3e499afa01ce0898ef1a79319231a803981781d
wendytaya <wendy.taya@ucsp.edu.pe>
2016-03-26 21:50:14 -0500

    Update ejercicio1.10.c

--- Ejercicios 1.5-1.10/ejercicio1.10.c
+++ Ejercicios 1.5-1.10/ejercicio1.10.c
@@ -1,3 +1,7 @@
+/*Write a program to copy its input to its output, 
+replacing eachtab by \t, each backspace by \b,
+ and each backslash by \\.This make tabs and backspaces
+ visible in an unambiguous way  */
 #include<stdio.h>
 
 main()
